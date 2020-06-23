8a1a40b7d02df0343bbae3aca0762144adab8605
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2020-06-23 18:44:18 +0600

    Update rel.py

--- rel.py
+++ rel.py
@@ -1,19 +1,49 @@
 from math import sqrt
 
 class SpecialRelativity:
-    def __init__(self,v,t_0,m_0,l_0):
-        self.v = v # velocity
-        self.t_0 = t_0 # Proper time 
-        self.m_0 = m_0 # Rest mass
-        self.l_0 = l_0 # Proper length
+
+    def __init__(self,v):
+        self.v = v # Velocity
         self.c = 3e8 # speed of causalty
-        self.k = sqrt(1-v**2/c**2) # Lorentz factor
+        self.k = sqrt(1-self.v**2/self.c**2) # Lorentz factor
         
 class TimeDilation(SpecialRelativity):
-    def __init__(self,v,t_0):
-        super().__init__(v,t_0)
-    
-    def t_dil(self):
-        t = t_0/k 
-        return round(t,5)
+
+    def __init__(self, v, t_0):
+        super().__init__(v)
+        self.v = v
+        self.t_0 = t_0
         
+    def t_dil(self):
+        t = self.t_0/self.k 
+        return round(t,5)
+
+class LengthContraction(SpecialRelativity):
+
+    def __init__(self,v,l_0):
+        super().__init__(v)
+        self.v = v
+        self.l_0 = l_0
+        
+    def l_cont(self):
+        l = self.l_0*self.k
+        return round(l,5)
+
+class MassIncrease(SpecialRelativity):
+
+    def __init__(self,v,m_0):
+        super().__init__(v)
+        self.v = v
+        self.m_0 = m_0
+        
+    def mass_inc(self):
+        m = self.m_0/self.k
+        return round(m,5)        
+
+event1 = TimeDilation(2e8,4)
+event2 = LengthContraction(2e7,5.2)
+event3 = MassIncrease(2.7e8,50)
+
+print(event1.t_dil())
+print(event2.l_cont())
+print(event3.mass_inc())
