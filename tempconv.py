9e1ce31fb01b0f1cab2b03c85a6ab24bd9a2885c
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2020-07-26 16:42:26 +0600

    Update tempconv.py

--- tempconv.py
+++ tempconv.py
@@ -1,31 +1,51 @@
 class Temperature:
 
-    def __init__(self,t):
+    def __init__(self,t,unit):
         self.t = t
+        self.unit = unit
         
     def celsius_to_fahrenheight(self):
-        f = 9*(self.t)/5 + 32
-        return round(f,2)
-
+        if self.unit == "c":
+            f = 9*(self.t)/5 + 32
+            return round(f,2)
+        else:
+            raise ValueError("Wrong Unit.")
+        
     def celsius_to_kelvin(self):
-        k = self.t + 273
-        return round(k,2)
+        if self.unit == "c":
+            k = self.t + 273
+            return round(k,2)
+        else:
+            raise ValueError("Wrong Unit.")    
 
     def fahrenheight_to_celsius(self):
-        c = 5*(self.t-32)/9
-        return round(c,2)
+        if self.unit == "f":
+            c = 5*(self.t-32)/9
+            return round(c,2)
+        else:
+            raise ValueError("Wrong Unit.")
+        
 
     def fahrenheight_to_kelvin(self):
-        k = 5*(self.t-32)/9 + 273
-        return round(k,2)
+        if self.unit == "f":
+            k = 5*(self.t-32)/9 + 273
+            return round(k,2)
+        else:
+            raise ValueError("Wrong Unit.")
 
     def kelvin_to_celsius(self):
-        c = self.t - 273
-        return round(c,2)
+        if self.unit == "k":
+            c = self.t - 273
+            return round(c,2)
+        else:
+            raise ValueError("Wrong Unit.")
 
     def kelvin_to_fahrenheight(self):
-        f = 9*(self.t)/5 + 32            
-        return round(f,2)
+        if self.unit == "k":
+            f = 9*(self.t)/5 + 32            
+            return round(f,2)
+        else:
+            raise ValueError("Wrong Unit.")
 
-t1 = Temperature(32)
-print(t1.celsius_to_kelvin())        
+t1 = Temperature(32,'c')
+print(t1.celsius_to_kelvin()) 
