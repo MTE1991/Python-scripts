6271b63f90f34c257226ccfa1bf4cf0472d9c613
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2020-08-13 13:10:50 +0600

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,37 +1,43 @@
 from random import choice
 
-rps = ["Rock", "Paper", "Scissor"]
+def rps_game():
+    
+    rps = ["Rock", "Paper", "Scissor"]
+    
+    print("\t -: Rock, Paper, Scissor game :-")
+    
+    user_choice = str(input("""Choose one of these three:
+    A. Rock
+    B. Paper
+    C. Scissor
+    Your choice: """))
+    
+    if user_choice == "A":
+      r = rps[0]
+      c = choice(rps)
+      print("Opponent's choice,", c)
+      if rps[0] == c:
+        print("You won!")
+      else: print("You lost!")
+    
+    elif user_choice == "B":
+      r = rps[0]
+      c = choice(rps)
+      print("Opponent choice,", c)
+      if rps[0] == c:
+        print("You won!")
+      else: print("You lost!")
+    
+    elif user_choice == "C":
+      r = rps[0]
+      c = choice(rps)
+      print("Opponent choice,", c)
+      if rps[0] == c:
+        print("You won!")
+      else: print("You lost!")
 
-print("\t -: Rock, Paper, Scissor game :-")
+user_choice_2 = str(input("Do you want to play again? (Y/N) :"))
 
-user_choice = str(input("""Choose one of these three:
-
-A. Rock
-B. Paper
-C. Scissor
-
-Your choice: """))
-
-if user_choice == "A":
-  r = rps[0]
-  c = choice(rps)
-  print("Opponent's choice,", c)
-  if rps[0] == c:
-    print("You won!")
-  else: print("You lost!")
-
-elif user_choice == "B":
-  r = rps[0]
-  c = choice(rps)
-  print("Opponent choice,", c)
-  if rps[0] == c:
-    print("You won!")
-  else: print("You lost!")
-
-elif user_choice == "C":
-  r = rps[0]
-  c = choice(rps)
-  print("Opponent choice,", c)
-  if rps[0] == c:
-    print("You won!")
-  else: print("You lost!")
+if user_choice_2 == "Y": rps_game()
+else: print("Thank you for playing Rock Paper Scissors with random module.")
+    
