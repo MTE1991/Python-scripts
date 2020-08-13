aa9090836bb4d8b2d36a6295a5e75ad6b0a326fd
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2020-08-13 13:03:56 +0600

    Create rps.py

---
+++ rps.py
@@ -0,0 +1,37 @@
+from random import choice
+
+rps = ["Rock", "Paper", "Scissor"]
+
+print("\t -: Rock, Paper, Scissor game :-")
+
+user_choice = ("""Choose one of these three:
+
+A. Rock
+B. Paper
+C. Scissor
+
+Your choice: """)
+
+if user_choice == "A":
+  r = rps[0]
+  c = choice(rps)
+  print("Opponent choose,", c)
+  if rps[0] == c:
+    print("You won!")
+  else: print("You lost!")
+
+elif user_choice == "B":
+  r = rps[0]
+  c = choice(rps)
+  print("Opponent choose,", c)
+  if rps[0] == c:
+    print("You won!")
+  else: print("You lost!")
+
+elif user_choice == "C":
+  r = rps[0]
+  c = choice(rps)
+  print("Opponent choose,", c)
+  if rps[0] == c:
+    print("You won!")
+  else: print("You lost!")
