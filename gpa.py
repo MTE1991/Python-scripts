7818d327d8642fa762ef1cccd3e99c7cb5cb0ac7
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2020-08-24 16:49:09 +0600

    Update gpa.py

--- gpa.py
+++ gpa.py
@@ -1,37 +1,49 @@
 class Student:
-    def __init__(self,year,exam):
-        self.year = int(year)
-        self.exam = exam
-        self.subjects = ["Mathematics","Physics","Chemistry","Biology",
-        "English","Bangla","ICT"]
-        self.marks = [] # marks to be appended later
+	def __init__(self,year,exam):
+		self.year = int(year)
+		self.exam = exam
+		self.subjects = ["Mathematics","Physics","Chemistry","Biology",
+		"English","Bangla","ICT"]
+		self.marks = [] # marks & gpa to be appended later
+		self.gpa = []
 
-    def getmarks(self):
-        for i in range(0,6):
-            print(i,self.subjects[i],"=")
-            n = int(input())
-            self.marks.append(n)
-            
-	def gpa(self):
-		if self.marks >= 33 and self.marks <= 39:
-			return 1.00
-		elif self.marks >= 40 and self.marks <= 49:
-			return 2.00
-		elif self.marks >= 50 and self.marks <= 59:
-			return 3.00
-		elif self.marks >= 60 and self.marks <= 69:
-			return 3.50
-		elif self.marks >= 70 and self.marks <= 79:
-			return 4.00
-		elif self.marks >= 80:
-			return 5.00
+	def get_marks(self):
+		for i in range(0,7):
+			print(i+1, self.subjects[i])
+			n = int(input(">> "))
+			self.marks.append(n)
+
+			if n >= 33 and n <= 39:
+				self.gpa.append(1.00)
+			elif n >= 40 and n <= 49:
+				self.gpa.append(2.00)
+			elif n >= 50 and n <= 59:
+				self.gpa.append(3.00)
+			elif n >= 60 and n <= 69:
+				self.gpa.append(3.50)
+			elif n >= 70 and n <= 79:
+				self.gpa.append(4.00)
+			elif n >= 80:
+				self.gpa.append(5.00)
 			
-		#Error handling:	
-		elif self.marks < 33:
-			raise ValueError("Fail.")
-		elif self.marks > 100:
-			raise ValueError("Marks can't exceed 100.")
+			#Error handling:	
+			elif n < 33:
+				raise ValueError("Fail.")
+			elif n > 100:
+				raise ValueError("Marks can't exceed 100.")
+			
+	def show_marks(self):
+		print(self.marks)
+		print(self.gpa)
+	
+	def result(self):
+		total = sum(self.marks)
+		gpa = sum(self.gpa)/7
+
+		print("Total marks = ", total)
+		print("GPA", gpa)
 
 s1 = Student(2020,"HSC")
-s1.getmarks()
-s1.gpa()
+s1.get_marks()
+s1.show_marks()
+s1.result()
